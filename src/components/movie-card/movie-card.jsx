9b270b1880b05f8f2701f54f9ceb3485341933c4
Justin Eastin <worldmisanthropy199@gmail.com>
2022-01-31 19:30:39 -0500

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -14,7 +14,13 @@ export class MovieCard extends React.Component {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    Title: PropTypes.string
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.string.isRequired,
+    Actors: PropTypes.string.isRequired,
+    Rating: PropTypes.string.isRequired,
+    Release: PropTypes.string.isRequired,
+    Imagepath: PropTypes.string.isRequired
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
 };
