4bc335adc75536997c3fe0f5e7fa7a59eddfd26c
Justin Eastin <worldmisanthropy199@gmail.com>
2022-02-02 09:40:42 -0500

    Update registration-view.jsx

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -18,4 +18,48 @@ export function RegistrationView(props) {
     props.onRegistration(username);
   };
 
-  return ();
+  return (
+    <Container>
+      <Row>
+          <Col>
+              <CardGroup>
+                  <Card>
+                      <Card.Body>
+                          <Card.Title>Register new account.</Card.Title>
+                          <Form>
+                              <Form.Group>
+                                  <Form.Label>Username:</Form.Label>
+                                  <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Enter username" />
+                                  //validation error here
+                                  {usernameErr && <p>{usernameErr}</p>}
+                              </Form.Group>
+
+                              <Form.Group>
+                                  <Form.Label>Password:</Form.Label>
+                                  <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} minlength="8" placeholder="Your password must be 8 or more characters" />
+                                  //validation error here
+                                  {passwordErr && <p>{passwordErr}</p>}
+                              </Form.Group>
+
+                              <Form.Group>
+                                  <Form.Label>Email:</Form.Label>
+                                  <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Enter email" />
+                                  //validation error here
+                                  {emailErr && <p>{emailErr}</p>}
+                              </Form.Group>
+
+                              <Form.Group>
+                                  <Form.Label>Birthday:</Form.Label>
+                                  <Form.Control type="date" value={birthday} onChange={e => setBirthday(e.target.value)} placeholder="Enter birthday" />
+                              </Form.Group>
+
+                              <Button variant="outline-light" type="submit" onClick={handleSubmit}>Submit</Button>
+                          </Form>
+                      </Card.Body>
+                  </Card>
+              </CardGroup>
+          </Col>
+      </Row>
+    </Container>
+  );
+}
