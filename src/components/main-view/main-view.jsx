fab422e70bf9ade1afea3bb5300251c2ab8f2f9a
Justin Eastin <worldmisanthropy199@gmail.com>
2022-02-04 12:23:36 -0500

    browser
    React router dom loaded

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,11 +3,14 @@ import axios from 'axios';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import './main-view.scss';
 
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { RegistrationView } from '../registration-view/registration-view';
 
 export class MainView extends React.Component {
 
@@ -18,6 +21,21 @@ export class MainView extends React.Component {
       selectedMovie: null,
       user: null
     };
+  }
+
+  getMovies(token) {
+    axios.get('https://drixflix.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}`}
+    })
+    .then(response => {
+      //assign result to state
+      this.setState({
+        movies:response.data
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 
   componentDidMount(){
@@ -39,40 +57,63 @@ export class MainView extends React.Component {
     });
   }
 
-  onLoggedIn(user) {
+  onLoggedIn(authData) {
+    console.log(authData);
     this.setState({
-      user
+      user: authData.user.Username
+    });
+
+    localStorage.setItem('token', authData.token);
+    localStorage.setItem('user', authData.user.Username);
+    this.getMovies(authData.token);
+  }
+
+  componentDidMount() {
+    let accessToken = localStorage.getItem('token');
+    if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem('user')
+      });
+      this.getMovies(accessToken);
+    }
+  }
+
+  onLoggedOut() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.setState({
+      user: null
     });
   }
 
   render() {
     const { movies, selectedMovie, user } = this.state;
 
-    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
-
+    if (!user) return <Row>
+      <Col>
+        <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+      </Col>
+    </Row>
     if (movies.length === 0) return <div className="main-view" />;
 
     return (
-      <div className="main-view">
-        {selectedMovie
-          ? (
-            <Row className="justify-content-md-center">
-              <Col md={8}>
-             <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+      <Router>
+        <Row className="main-view justify-content-md-center">
+          <Route exact Path="/" render={() => {
+            return movies.map(m => (
+              <Col md={3} key={m._id}>
+                <MovieCard movie={m} />
               </Col>
-            </Row>
-          )
-          : (
-            <Row className="justify-content-md-center">
-             {movies.map(movie => (
-               <Col md={3} key={movie._id}>
-                 <MovieCard movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie); }} />
-               </Col>
-              ))}
-            </Row>
-          )
-        }
-      </div>
-    );
-  }
+            ))
+          }} />
+          <Route path="movies/:movieId" render={({ match }) => {
+            return <Col md={8}>
+            <MovieView movie={movies.find(m => m._id === match.params.movieId)} />
+          </Col>
+        }} />
+
+      </Row>
+    </Router>
+   );
+ }
 }
