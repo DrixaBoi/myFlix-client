c239f802c86bf9a282440b49fe256c5fbd870019
Justin Eastin <worldmisanthropy199@gmail.com>
2022-02-04 14:10:08 -0500

    export import
    exporting and importing routes and links

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import axios from 'axios';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import './main-view.scss';
-
+import axios from 'axios';
+import {Container, Row, Col} from 'react-bootstrap';
+import { Menubar } from '../navbar/navbar';
+import { RegistrationView } from '../registration-view/registration-view';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
-import { RegistrationView } from '../registration-view/registration-view';
+import { DirectorView } from '../director-view/director-view';
+import { GenreView } from '../genre-view/genre-view';
+import { ProfileView } from '../profile-view/profile-view';
+import { UserUpdate } from '../profile-view/update-view';
+import './main-view.scss';
 
 export class MainView extends React.Component {
 
@@ -18,9 +19,29 @@ export class MainView extends React.Component {
     super();
     this.state = {
       movies: [],
-      selectedMovie: null,
       user: null
     };
+  }
+
+  componentDidMount() {
+    let accessToken = localStorage.getItem('token');
+    if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem('user')
+      });
+      this.getMovies(accessToken);
+    }
+  }
+
+  onLoggedIn(authData) {
+    console.log(authData);
+    this.setState({
+      user: authData.user.Username
+    });
+
+    localStorage.setItem('token', authData.token);
+    localStorage.setItem('user', authData.user.Username);
+    this.getMovies(authData.token);
   }
 
   getMovies(token) {
@@ -57,27 +78,6 @@ export class MainView extends React.Component {
     });
   }
 
-  onLoggedIn(authData) {
-    console.log(authData);
-    this.setState({
-      user: authData.user.Username
-    });
-
-    localStorage.setItem('token', authData.token);
-    localStorage.setItem('user', authData.user.Username);
-    this.getMovies(authData.token);
-  }
-
-  componentDidMount() {
-    let accessToken = localStorage.getItem('token');
-    if (accessToken !== null) {
-      this.setState({
-        user: localStorage.getItem('user')
-      });
-      this.getMovies(accessToken);
-    }
-  }
-
   onLoggedOut() {
     localStorage.removeItem('token');
     localStorage.removeItem('user');
@@ -87,32 +87,62 @@ export class MainView extends React.Component {
   }
 
   render() {
-    const { movies, selectedMovie, user } = this.state;
-
-    if (!user) return <Row>
-      <Col>
-        <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
-      </Col>
-    </Row>
-    if (movies.length === 0) return <div className="main-view" />;
+    const { movies, user } = this.state;
 
     return (
       <Router>
-        <Row className="main-view justify-content-md-center">
-          <Route exact Path="/" render={() => {
-            return movies.map(m => (
-              <Col md={3} key={m._id}>
-                <MovieCard movie={m} />
+        <Menubar user={user} />
+        <Container>
+          <Row className="main-view justify-content-md-center">
+            <Route exact Path="/" render={() => {
+              if (!user) return <Col>
+                <LoginView onLoggedIn={user => this.onLoggedIn(user)} />
               </Col>
-            ))
-          }} />
-          <Route path="movies/:movieId" render={({ match }) => {
-            return <Col md={8}>
-            <MovieView movie={movies.find(m => m._id === match.params.movieId)} />
-          </Col>
-        }} />
+              if (movies.length ===0) return <div className="mainview">
 
-      </Row>
+              return movies.map(m => (
+                <Col md={3} key={m._id}>
+                  <MovieCard movie={m} />
+                </Col>
+              ))
+            }} />
+            <Route path="/register" render={() => {
+              if (user) return <Redirect to ="/" />
+              return <Col>
+                <RegistrationView />
+              </Col>
+            }} />
+            <Route path="movies/:movieId" render={({ match, history }) => {
+              return <Col md={8}>
+              <MovieView movie={movies.find(m => m._id === match.params.movieId)} onBackClick={() => history.goBack()} />
+            </Col>
+            }} />
+            <Route path="/directors/:name" render={({ match, history }) => {
+            if (movies.length === 0) return <div className="main-view" />;
+              return <Col md={8}>
+              <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director} onBackclick={() => history.goBack()} />
+            </Col>
+            }} />
+            <Route path="/genres/:name" render={({ match, history }) => {
+            if (movies.length === 0) return <div className="main-view" />;
+              return <Col md={8}>
+              <GenreView genre={movies.find(m => m.Genre.Name === match.params.name).Genre} onBackclick={() => history.goBack()} />
+            </Col>
+            }} />
+            <Route path={`/users/${user}`} render={({ match, history }) => {
+            if (!user) return <Redirect to="/" />
+              return <Col>
+              <ProfileView movies={movies} user={user} onBackClick={() => history.goBack()}/>
+              </Col>
+            }} />
+            <Route path={`/user-update/${user}`} render={({ match, history }) => {
+            if (!user) return <Redirect to="/" />
+              return <Col>
+              <UserUpdate user={user} onBackClick={() => history.goBack()}/>
+              </Col>
+            }} />
+        </Row>
+      </Container>
     </Router>
    );
  }
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,65 +1,131 @@
 import React, { useState } from 'react';
 import PropTypes from "prop-types";
+import axios from 'axios';
+import {Row, Col, Button, Container, Form } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
 import './registration-view.scss';
 
 export function RegistrationView(props) {
+  const [ name, setName ] = useState('');
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
   const [ email, setEmail ] = useState('');
-  const [ Birthday, setBirthday] = useState('');
+  const [ birthday, setBirthday ] = useState('');
+  const [ values, setValues ] = useState({
+    nameErr: '',
+    usernameErr: '',
+    passwordErr: '',
+    emailErr: '',
+  });
 
+  const validate = () => {
+    let iReq = true;
+    if(!name){
+      serValues({...values, nameErr: 'Name is required'});
+      isReq = false;
+    }
+    if(!username){
+      setValues({...values, usernameErr: 'Username is required'});
+      isReq = false;
+    } else if(username.length < 5){
+      setValues({...values, usernameErr: 'Username must be 5 characters long'});
+      isReq = false;
+    }
+    if(!password){
+      setValues({...values, passwordErr: 'Password required'});
+      isReq = false;
+    } else if(password.length < 6){
+      setValues({...values, passwordErr: 'Password must be 6 characters long'});
+      isReq = false;
+    }
+    if(!email) {
+      setValues({...values, emailErr: 'Email required'});
+      isReq = false;
+    } else if(email.indexOf('@') === -1) {
+      setValues({...values, emailErr: 'Email is not valid'});
+      isReq = false;
+    }
+
+    return isReq;
+  }
+}
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password, email, Birthday);
-    /* Send a request to the server for authentication */
-    /* then call props on registored user(username) */
-    props.onRegistration(username);
+    const isReq = validate();
+    if(isReq){
+      axios.post('https://drixflix.herokuapp.com/', {
+        Name: name,
+        Username: username,
+        Password: password,
+        Email: email,
+        Birthday: birthday
+      })
+      .then(response => {
+        const data = response.data;
+        console.log(data);
+        alert('Registration successful!');
+        window.open('/', '_self');
+        /* '_self' is necessary so the page will open in the current tab*/
+      })
+      .catch(response => {
+        console.error(response);
+        alert('unable to register');
+      });
+    }
   };
 
   return (
-    <Container>
-      <Row>
-          <Col>
-              <CardGroup>
-                  <Card>
-                      <Card.Body>
-                          <Card.Title>Register new account.</Card.Title>
-                          <Form>
-                              <Form.Group>
-                                  <Form.Label>Username:</Form.Label>
-                                  <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} placeholder="Enter username" />
-                                  //validation error here
-                                  {usernameErr && <p>{usernameErr}</p>}
-                              </Form.Group>
+      <Row className="mt-5">
+      <Col md={12}>
+        <Form>
+          <h3>Sign Up</h3>
+          <p></p>
+          <Form.Group controlId="formUsername" className="reg-form-inputs">
+            <Form.Label>Username:</Form.Label>
+            <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            {values.usernameErr && <p>{values.usernameErr}</p>}
+          </Form.Group>
 
-                              <Form.Group>
-                                  <Form.Label>Password:</Form.Label>
-                                  <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} minlength="8" placeholder="Your password must be 8 or more characters" />
-                                  //validation error here
-                                  {passwordErr && <p>{passwordErr}</p>}
-                              </Form.Group>
+          <Form.Group controlId="formName" className="reg-form-inputs">
+            <Form.Label>Name:</Form.Label>
+            <Form.Control type="text" value={name} onChange={e => setName(e.target.value)} />
+            {values.nameErr && <p>{values.nameErr}</p>}
+          </Form.Group>
 
-                              <Form.Group>
-                                  <Form.Label>Email:</Form.Label>
-                                  <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Enter email" />
-                                  //validation error here
-                                  {emailErr && <p>{emailErr}</p>}
-                              </Form.Group>
+          <Form.Group controlId="formPassword" className="reg-form-inputs">
+            <Form.Label>Password:</Form.Label>
+            <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            {values.passwordErr && <p>{values.passwordErr}</p>}
+          </Form.Group>
 
-                              <Form.Group>
-                                  <Form.Label>Birthday:</Form.Label>
-                                  <Form.Control type="date" value={birthday} onChange={e => setBirthday(e.target.value)} placeholder="Enter birthday" />
-                              </Form.Group>
+          <Form.Group controlId="Email" className="reg-form-inputs">
+            <Form.Label>Email:</Form.Label>
+            <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} />
+            {values.emailErr && <p>{values.emailErr}</p>}
+          </Form.Group>
 
-                              <Button variant="outline-light" type="submit" onClick={handleSubmit}>Submit</Button>
-                          </Form>
-                      </Card.Body>
-                  </Card>
-              </CardGroup>
-          </Col>
-      </Row>
-    </Container>
-  );
-}
+          <Form.Group controlId="updateBirthday">
+            <Form.Label>Birthday</Form.Label>
+            <Form.Control type="date" name="birthday" onChange={(e) => setBirthday(e.target.value)} />
+          </Form.Group>
+
+          <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+          <p></p>
+          <p>Already registered? <Link to={'/'}>Sign in</Link> here</p>
+        </Form>
+      </Col>
+    </Row>
+
+    );
+
+
+RegistrationView.propTypes = {
+  egister: PropTypes.shape({
+  Name: PropTypes.string.isRequired,
+  Username: PropTypes.string.isRequired,
+  Password: PropTypes.string.isRequired,
+  Email: PropTypes.string.isRequired
+  }),
+};
