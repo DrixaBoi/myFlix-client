78b7704aefd6526618ec80d50ef638c814d1e1ac
Justin Eastin <worldmisanthropy199@gmail.com>
2022-02-09 13:23:12 -0500

    Update movies-list.jsx

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -22,7 +22,7 @@ function MoviesList(props) {
 
 return <>
   <Col md={12} style={{ margin: '1em' }}>
-    <VisibilityFilterInput visibilityfilter={visibilityFilter} />
+    <VisibilityFilterInput visibilityFilter={visibilityFilter} />
   </Col>
   {filteredMovies.map(m => (
       <Col md={3} key={m._id}>
